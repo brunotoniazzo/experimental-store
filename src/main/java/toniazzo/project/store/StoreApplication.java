23eda1152f7ac458f8bc94129c7efac6dd78c321
Bruno Toniazzo <bruno.toniazzo@wansoft.com.br>
2023-10-23 20:18:19 -0300

    Project Store 1st Commit
    Commiting the first configs of the project, nothing really special.

---
+++ src/main/java/toniazzo/project/store/StoreApplication.java
@@ -0,0 +1,20 @@
+package toniazzo.project.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@GetMapping(value = "")
+	public void helloWorld() {
+		System.out.println("Hello, World!");
+	}
+}
