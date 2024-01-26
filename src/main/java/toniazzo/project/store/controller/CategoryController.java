b4569b533726f0d447dd9e5160d158fbaff391b9
Bruno Toniazzo <bruno.toniazzo@wansoft.com.br>
2024-01-26 10:13:58 -0300

    Update Maven dependencies and Config application.properties

--- src/main/java/toniazzo/project/store/controller/CategoryController.java
+++ src/main/java/toniazzo/project/store/controller/CategoryController.java
@@ -1,11 +1,20 @@
 package toniazzo.project.store.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import toniazzo.project.store.service.CategoryService;
 
 @Controller
 @RequestMapping("category")
 public class CategoryController {
+
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     public void saveCategory() {
 
