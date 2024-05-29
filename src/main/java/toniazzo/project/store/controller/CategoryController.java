ca8610284c4e9cee24b910702b1f33e2ed8de790
Bruno Toniazzo <btoniazzo011@gmail.com>
2024-05-29 00:03:48 -0300

    Add Https Response Status for all cases at Controllers

--- src/main/java/toniazzo/project/store/controller/CategoryController.java
+++ src/main/java/toniazzo/project/store/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package toniazzo.project.store.controller;
 
 import jakarta.transaction.Transactional;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import toniazzo.project.store.entity.Category;
 import toniazzo.project.store.service.CategoryService;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping(path = "/api/categories/")
@@ -20,29 +24,33 @@ public class CategoryController {
     }
 
     @GetMapping
-    List<Category> getAllCategories() {
+    @ResponseStatus(HttpStatus.OK)
+    public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
 //    @GetMapping
+//    @ResponseStatus(HttpStatus.OK)
 //    Page<Category> getAllCategoriesByPage(Pageable pageable) {
 //        return categoryService.getAllCategoriesByPage(pageable);
 //    }
 
     @PostMapping
     @Transactional
-    List<Category> saveCategory(@RequestBody Category category) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Category> saveCategory(@RequestBody Category category) {
         return categoryService.saveCategory(category);
     }
 
     @PutMapping
-    List<Category> updateCategory(@RequestBody Category category) {
+    @ResponseStatus(HttpStatus.OK)
+    public List<Category> updateCategory(@RequestBody Category category) {
         return categoryService.updateCategory(category);
     }
 
     @DeleteMapping("{id}")
-    List<Category> deleteCategory(@PathVariable("id") Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public List<Category> deleteCategory(@PathVariable("id") Long id) {
         return categoryService.deleteCategory(id);
     }
-
 }
--- src/main/java/toniazzo/project/store/controller/ProductController.java
+++ src/main/java/toniazzo/project/store/controller/ProductController.java
@@ -2,6 +2,7 @@ package toniazzo.project.store.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import toniazzo.project.store.entity.Product;
 import toniazzo.project.store.service.ProductService;
@@ -19,22 +20,26 @@ public class ProductController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @PostMapping
     @Transactional
+    @ResponseStatus(HttpStatus.CREATED)
     List<Product> saveProduct(@RequestBody @Valid Product product){
         return productService.saveProduct(product);
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.OK)
     List<Product> updateProduct(@RequestBody @Valid Product product){
         return productService.updateProduct(product);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     List<Product> deleteProduct(@PathVariable("id") Long id){
         return productService.deleteProduct(id);
     }
